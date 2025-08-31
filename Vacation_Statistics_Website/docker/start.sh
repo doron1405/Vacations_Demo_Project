3d103320b4f0cc4306ac9249a54a09426fab6791
Alexey Kozlov <99757449+AlexeyKoz@users.noreply.github.com>
2025-08-31 19:24:12 +0300

    start.sh_fixed

--- Vacation_Statistics_Website/docker/start.sh
+++ Vacation_Statistics_Website/docker/start.sh
@@ -6,4 +6,3 @@ echo "Starting Vacation Statistics Dashboard..."
 # Start supervisor to manage both nginx and Flask
 echo "Starting supervisor..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
-
